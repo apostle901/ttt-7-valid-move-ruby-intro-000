7a26c7bd57d77cba0bf4a1bba4e4504c0a49bf73
Jawann <jawann@gmail.com>
2017-10-22 21:37:58 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   return false if !(0..8).include?(index)
-  position_taken?(board, index)
+  !position_taken?(board, index)
 end
 
 def position_taken?(board, index)
